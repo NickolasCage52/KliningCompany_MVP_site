c3f7ddb8c164f67eb5ba6f3d9ca5f7e565b59c55
NickolasCage52 <NickolasCage52@users.noreply.github.com>
2026-03-12 00:56:41 +0000

    deploy: ab96eb87ddf8597db179fa53fdf6116d60e85829

---
+++ _next/static/chunks/app/articles/[id]/page-df8cc968c32a459c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[422,160,385,301],{7004:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7004)}),_N_E=n.O()}]);
